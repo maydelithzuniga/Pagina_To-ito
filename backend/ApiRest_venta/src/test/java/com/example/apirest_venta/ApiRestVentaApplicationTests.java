bf7be57ee9d33dc23ae4794f9070c4e7a359bb7d
maydelith <zunigacabreramaydelithlaura@gmail.com>
2026-09-12 21:00:33 -0500

    nuevo

---
+++ backend/ApiRest_venta/src/test/java/com/example/apirest_venta/ApiRestVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirest_venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestVentaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
